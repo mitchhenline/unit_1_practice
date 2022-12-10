d3e03dd22b9756bb57e2f93c0099af852a3dedb1
Mitch Henline <rmhenline@gmail.com>
2022-12-10 13:53:16 -0700

    more practice

--- main.py
+++ main.py
@@ -4,14 +4,14 @@
 def team_input():
     team1 = input('What is the name of the home team? -- ')
     try:
-        score1 = float(input('How many points did the home team score? -- '))
+        score1 = int(input('How many points did the home team score? -- '))
     except:
-        score1 = float(input('Please enter a number for how many points the home team scored -- '))
+        score1 = int(input('Please enter a number for how many points the home team scored -- '))
     team2 = input('What is the name of the visiting team? -- ')
     try:
-        score2 = float(input('How many points did the visiting team score? -- '))
+        score2 = int(input('How many points did the visiting team score? -- '))
     except:
-        score2 = float(input('Please enter a number for how many points the visiting team scored -- '))
+        score2 = int(input('Please enter a number for how many points the visiting team scored -- '))
 
     team_dictionary = {
         "home_team": team1,
@@ -20,6 +20,21 @@ def team_input():
         "visiting_score": score2
     }
 
+    if score1 > score2:
+        return f"{team1} beat {team2} by a score of {score1}-{score2}."
+    elif score2 > score1:
+        return f"{team2} beat {team1} by a score of {score2}-{score1}."
+    elif score1==score2:
+        return f"{team1} and {team2} tied with {score1} points each."
     return team_dictionary
 
-team_input()
+# print(team_input())
+
+
+mountainwest = {'usu':"aggies", "bsu":"broncos","wyoming":"cowboys"}
+
+
+# print(mountainwest.values())
+
+
+
